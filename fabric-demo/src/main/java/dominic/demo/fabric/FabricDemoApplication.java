7b6fae87a85d4677ee4ff7235458a666402cd4e7
dominiche <herongxing.me@foxmail.com>
2018-04-27 11:45:18 +0800

    first commit

---
+++ fabric-demo/src/main/java/dominic/demo/fabric/FabricDemoApplication.java
@@ -0,0 +1,12 @@
+package dominic.demo.fabric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabricDemoApplication.class, args);
+    }
+}
---
+++ fabric-demo/src/main/java/dominic/demo/fabric/config/NetworkConfiguration.java
@@ -0,0 +1,11 @@
+package dominic.demo.fabric.config;
+
+import lombok.Getter;
+import org.hyperledger.fabric.sdk.NetworkConfig;
+
+public class NetworkConfiguration {
+    @Getter
+    private NetworkConfig networkConfig;
+
+
+}
